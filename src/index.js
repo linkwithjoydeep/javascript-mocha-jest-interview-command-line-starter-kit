5085f712eb139ca97fc6f6422114e1b84a1b6063
Joydeep Bhattacharya <58521295+linkwithjoydeep@users.noreply.github.com>
2025-08-05 10:42:18 +0530

    feat: handle cli starter kit input

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 
-/*
-* This is the main entry point for the program. It will parse the input for you.
-* You don't need to change this.
-*/
 function main() {
-    const input = process.argv.slice(2)
-    if (input.length === 0) {
-        throw new Error("no command line arguments passed")
+    const commands = process.argv.slice(2)
+    if (commands.length === 0) {
+        throw new Error("No command line arguments passed")
     }
-    var output = handle(input[0])
-    console.log(output)
+    /*
+    * Format of the 'args' array: [`<COMMAND_NAME_1> <ARG1> <ARG2> .. <ARG N>`, `<COMMAND_NAME_2> <ARG1> <ARG2> .. <ARG N>`]
+    *  Example: ["PLACE_ORDER 101 Apple 5", "TOTAL_COST 101"]
+    *
+    * The code evaluator will execute this code by using the command
+    * node . 'PLACE_ORDER 101 Apple 5' 'TOTAL_COST 101'
+    *
+    * We loop through the list of commands passed in as input arguments and handle each one of them
+    */
+    for (let cmd of commands) {
+        //arg will have each command passed in the command line argumens
+        handle(cmd)
+    }
 }
 
 
 /*
- * Use this method to write your solution. 
- * Make sure to print out the solution.
+ * This method should parse each input and assigns it into different variables.
+ *
+ * The value of the function parameter "input" will be of the format - "`<COMMAND_NAME_1> <ARG1> <ARG2> .. <ARG N>".
+ * We split the string and retrieve the input data appropriately into the variable inputListForOneCommand.
+ *
  */
-function handle(input) {
-    //TODO: implement the logic to handle each input and return the finall output at the end of the function.
-    return input
+function handle(cmd) {
+    const inputsForOneCommand = cmd.split(' ')
+    console.log(inputsForOneCommand)
+    //TODO: implement the logic to handle each input
 }
-
 
 main()
